89faa1872e18c2e415c751d9916c92bb2e8fe01d
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-07 16:32:22 +0530

    Configured routing in the index.js file to initialize and use the routes for the user service.

--- Project/backend/UserService/index.js
+++ Project/backend/UserService/index.js
@@ -3,9 +3,13 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 
+var UserRoutes = require("./api/routes/User")
+
 var app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: "*" }))
 app.listen(4000, () => console.log("Server started at : 3500"))
+
+app.use("/User", UserRoutes)
 
 app.use(express.static("public"))
