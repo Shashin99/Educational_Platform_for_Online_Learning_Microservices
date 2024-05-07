59695fab0d9eed08966d7039c18b5973611fda6c
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-07 22:36:36 +0530

    Implement "get all" function in controller class for course service

--- Project/backend/CourseService/api/controllers/CourseController.js
+++ Project/backend/CourseService/api/controllers/CourseController.js
@@ -1,6 +1,16 @@
 const express = require("express");
 var { Course } = require("../models/Course");
 
+exports.getAll = async (req, res) => {
+    Course.find((err, docs) => {
+        if (!err) {
+            res.send(docs);
+        } else {
+            console.log(JSON.stringify(err, undefined, 2));
+        }
+    });
+};
+
 exports.newCourse = async (req, res) => {
     var newRecord = new Course({
         note1: req.body.note1,
