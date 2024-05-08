f2ccd32309b40b4576afb05b50b7016617a9a225
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-08 05:51:44 +0530

    Implement "get all" function in controller class for enroll service

--- Project/backend/EnrollService/api/controllers/EnrollController.js
+++ Project/backend/EnrollService/api/controllers/EnrollController.js
@@ -1,6 +1,17 @@
 const express = require("express");
 var { Enroll } = require("../models/Enroll");
 
+
+exports.getAll = async (req, res) => {
+    Enroll.find((err, docs) => {
+        if (!err) {
+            res.send(docs)
+        } else {
+            console.log(JSON.stringify(err, undefined, 2))
+        }
+    })
+}
+
 exports.newEnroll = async (req, res) => {
     var newRecord = new Enroll({
         course_id: req.body.course_id,
