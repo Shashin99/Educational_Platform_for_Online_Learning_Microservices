26ab9d55a670fcc195b5f97be04b475bbefb6aec
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-08 17:00:21 +0530

    Configured routing in the index.js file to initialize and use the routes for the learner service.

--- Project/backend/LearnerService/index.js
+++ Project/backend/LearnerService/index.js
@@ -1,11 +1,15 @@
-require("./configuration/db");
+require("./configuration/db.js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+var LearnerRoutes = require("./api/routes/Learner.js");
 
 var app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: "*" }));
 app.listen(4002, () => console.log("Server started at : 4002"));
+
+app.use("/Learner", LearnerRoutes);
 
 app.use(express.static("public"));
