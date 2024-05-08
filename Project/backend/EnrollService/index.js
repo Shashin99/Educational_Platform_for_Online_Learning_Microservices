bf6b17e96f6538422081a0f8463dbfe586abb344
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-08 08:42:59 +0530

    Configured routing in the index.js file to initialize and use the routes for the enroll service.

--- Project/backend/EnrollService/index.js
+++ Project/backend/EnrollService/index.js
@@ -1,11 +1,15 @@
-require("./configuration/db");
+require("./configuration/db.js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+var EnrollRoutes = require("./api/routes/Enroll.js");
 
 var app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: "*" }));
 app.listen(3500, () => console.log("Server started at : 3500"));
+
+app.use("/Enroll", EnrollRoutes);
 
 app.use(express.static("public"));
