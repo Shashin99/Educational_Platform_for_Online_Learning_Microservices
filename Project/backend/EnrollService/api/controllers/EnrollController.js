d7495d0e660553521585ba00e447536730e692cc
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-08 05:53:08 +0530

    Implement "get by ID" function in controller class for enroll service

--- Project/backend/EnrollService/api/controllers/EnrollController.js
+++ Project/backend/EnrollService/api/controllers/EnrollController.js
@@ -1,17 +1,30 @@
 const express = require("express");
+var ObjectID = require("mongoose").Types.ObjectId;
 var { Enroll } = require("../models/Enroll");
-
 
 exports.getAll = async (req, res) => {
     Enroll.find((err, docs) => {
         if (!err) {
-            res.send(docs)
+            res.send(docs);
         } else {
-            console.log(JSON.stringify(err, undefined, 2))
+            console.log(JSON.stringify(err, undefined, 2));
         }
-    })
-}
+    });
+};
 
+exports.getId = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send(req.params.id);
+    }
+
+    Enroll.findById(req.params.id, (err, docs) => {
+        if (!err) {
+            res.send(docs);
+        } else {
+            console.log(JSON.stringify(err, undefined, 2));
+        }
+    });
+};
 exports.newEnroll = async (req, res) => {
     var newRecord = new Enroll({
         course_id: req.body.course_id,
