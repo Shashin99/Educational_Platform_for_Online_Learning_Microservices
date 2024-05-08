2b01981467bc144257eb9086ecdbee4319d46ac8
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-08 05:32:39 +0530

    Configure database connection for enroll service

---
+++ Project/backend/EnrollService/index.js
@@ -0,0 +1,11 @@
+require("./configuration/db");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
+app.listen(3500, () => console.log("Server started at : 3500"));
+
+app.use(express.static("public"));
