054724022ba1e5ccaa5d91a4ab52386b8ae421c6
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-08 16:49:59 +0530

    Implement "get by ID" function in controller class for learner service

--- Project/backend/LearnerService/api/controllers/LearnerController.js
+++ Project/backend/LearnerService/api/controllers/LearnerController.js
@@ -12,6 +12,20 @@ exports.getAll = async (req, res) => {
     });
 };
 
+exports.getId = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send(req.params.id);
+    }
+
+    Learner.findById(req.params.id, (err, docs) => {
+        if (!err) {
+            res.send(docs);
+        } else {
+            console.log(JSON.stringify(err, undefined, 2));
+        }
+    });
+};
+
 exports.newLearner = async (req, res) => {
     let lFound = await Learner.findOne({
         course_id: req.body.course_id,
