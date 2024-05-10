9c343fc90ecf867126dee6b67baa203c4e68162d
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-10 10:02:37 +0530

    Implemented frontend functionality to send user verification data to the backend API for processing

--- Project/frontend/src/components/UserVerification.js
+++ Project/frontend/src/components/UserVerification.js
@@ -9,6 +9,108 @@ import { useHistory } from "react-router-dom"
 
 function UserVerification() {
 
+  const [code, setCode] = useState('');
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+  const inputRef = React.useRef();
+
+  useEffect(() => {
+    console.log(history.location.state.email)
+    setEmail(history.location.state.email)
+  });
+
+  const setCodeForm = (e) => {
+    setCode(e.target.value)
+  }
+
+  const onClear = () => {
+    setCode('')
+    inputRef.current.focus()
+  }
+
+  const validation = () => {
+    var Error = false;
+
+    if (code === "") {
+      ButterToast.raise({
+        content: <Cinnamon.Crisp title="Validation Error!"
+          content="Code Required!"
+          scheme={Cinnamon.Crisp.SCHEME_RED}
+          icon={<ErrorOutlineIcon />}
+        />
+      })
+      Error = true;
+    }
+
+    if (Error) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const SubmitForm = async (e) => {
+    e.preventDefault();
+
+    if (validation()) {
+
+      const url = "http://localhost:4001/User/otp";
+      const data = JSON.stringify({
+        email: email,
+        code: code,
+      });
+      console.log(data);
+      
+      await axios
+        .post(url, data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then(async(res) => {
+          console.log(res.data.err);
+          if(res.data.err!=="User not found"){
+            if(res.data.err==="Incorrect Code"){
+              onClear()
+              ButterToast.raise({
+                content: <Cinnamon.Crisp title="Incorrect Error!"
+                content="Incorrect Code!"
+                  scheme={Cinnamon.Crisp.SCHEME_RED}
+                  icon={<ErrorOutlineIcon />}
+                />
+              })
+            }else if(res.data.err==="Try Again"){
+              ButterToast.raise({
+                content: <Cinnamon.Crisp title="Connection Error!"
+                  content="Try Again!"
+                  scheme={Cinnamon.Crisp.SCHEME_RED}
+                  icon={<ErrorOutlineIcon />}
+                />
+              })
+            }else{
+              ButterToast.raise({
+                content: <Cinnamon.Crisp title="Success!"
+                  content="Account Verify Successful!"
+                  scheme={Cinnamon.Crisp.SCHEME_GREEN}
+                  icon={<CheckCircleOutlineIcon />}
+                />
+              })
+              history.push("/login")
+            }
+          }else{
+            ButterToast.raise({
+              content: <Cinnamon.Crisp title="Data Error!"
+                content="User not found!"
+                scheme={Cinnamon.Crisp.SCHEME_RED}
+                icon={<ErrorOutlineIcon />}
+              />
+            })
+          }
+        }).catch(function(error) {
+          console.log(error);
+        })
+
+
+    }
+  };
 
   return (
     <div className="App">
@@ -23,7 +125,7 @@ function UserVerification() {
               OTP Code
             </Typography>
             <br />
-            <form autoComplete="off">
+            <form autoComplete="off" onSubmit={SubmitForm}>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <TextField type="email"
@@ -33,7 +135,7 @@ function UserVerification() {
                     label="Email"
                     variant="outlined"
                     name="email"
-                    
+                    value={email}
                     fullWidth />
                 </Grid>
                 <Grid item xs={12}>
@@ -44,7 +146,8 @@ function UserVerification() {
                     label="OTP Code"
                     variant="outlined"
                     name="code"
-                   
+                    value={code}
+                    onChange={setCodeForm}
                     fullWidth />
                 </Grid>
                 <Grid item xs={12}>
@@ -55,7 +158,7 @@ function UserVerification() {
                   <Button
                     variant="contained"
                     color="secondary"
-                
+                    onClick={() => onClear()}
                     fullWidth
                   >Clear</Button>
                 </Grid>
