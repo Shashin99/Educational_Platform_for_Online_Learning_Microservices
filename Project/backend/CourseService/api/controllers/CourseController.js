ad4eb927fe56c707b41017c36f1a44003cfc8357
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2024-05-13 12:12:50 +0530

    Update Course Controller of the course service

--- Project/backend/CourseService/api/controllers/CourseController.js
+++ Project/backend/CourseService/api/controllers/CourseController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 var ObjectID = require("mongoose").Types.ObjectId;
 var { Course } = require("../models/Course");
+var multer = require("multer");
+var uniqid = require("uniqid");
 
 exports.getAll = async (req, res) => {
     Course.find((err, docs) => {
@@ -28,11 +30,13 @@ exports.getId = async (req, res) => {
 
 exports.newCourse = async (req, res) => {
     var newRecord = new Course({
+        name: req.body.name,
         note1: req.body.note1,
         note2: req.body.note2,
         note3: req.body.note3,
         note4: req.body.note4,
         note5: req.body.note5,
+        price: req.body.price,
         video_link: req.body.video_link,
         quiz_details: req.body.quiz_details,
     });
@@ -53,11 +57,13 @@ exports.editCourse = async (req, res) => {
     }
 
     var updateRecords = {
+        name: req.body.name,
         note1: req.body.note1,
         note2: req.body.note2,
         note3: req.body.note3,
         note4: req.body.note4,
         note5: req.body.note5,
+        price: req.body.price,
         video_link: req.body.video_link,
         quiz_details: req.body.quiz_details,
     };
@@ -87,5 +93,27 @@ exports.deleteCourse = async (req, res) => {
         } else {
             res.send(err);
         }
+    });
+};
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public");
+    },
+    filename: function (req, file, cb) {
+        cb(null, uniqid() + "-" + file.originalname);
+    },
+});
+
+var upload = multer({ storage: storage }).single("file");
+
+exports.uploadFile = async (req, res) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err);
+        } else if (err) {
+            return res.status(500).json(err);
+        }
+        return res.status(200).send(req.file);
     });
 };
